4a8e32bb28f6bfe3793acd4a82453369da1198d1
Georges Labrèche <georges@opendatakosovo.org>
2017-09-08 04:26:29 +0200

    Update get_unit_vect.m

--- get_unit_vect.m
+++ get_unit_vect.m
@@ -1,7 +1,10 @@
-function [ unit_vect ] = get_unit_vect( lambda_subbed )
+function [ unit_vect ] = get_unit_vect(JJ, lambda)
 %GET_UNIT_VECT Summary of this function goes here
 %   Detailed explanation goes here
 % Subsitute these back in JJ-lambda*eye(3)
+
+syms x
+lambda_subbed = subs(JJ-x*eye(3), x, lambda);
 
 syms y z
 solved_x = 1;
@@ -11,6 +14,6 @@ fun = lambda_subbed * X == [0; 0; 0];
 fun_sol = solve([fun(2), fun(3)], [y, z]);
 
 syms a b c
-unit_vect = (solved_x*a + fun_sol.y*b + fun_sol.z*c) / sqrt(solved_x^2 +  fun_sol.y^2 + fun_sol.z^2);
+unit_vect = (solved_x*a + fun_sol.y*b + fun_sol.z*c) / sqrt((solved_x)^2 +  (fun_sol.y)^2 + (fun_sol.z)^2);
 
 end
